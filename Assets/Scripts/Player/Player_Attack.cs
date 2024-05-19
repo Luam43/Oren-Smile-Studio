dc900e143a4d03cb24b1060b790740e515d5908b
JoeyKagan <jolova224@gmail.com>
2024-05-19 23:54:32 +0700

    Attack damage test

--- Assets/Scripts/Player/Player_Attack.cs
+++ Assets/Scripts/Player/Player_Attack.cs
@@ -6,6 +6,11 @@ public class Player_Attack : MonoBehaviour
 {
     public  Animator animator;
 
+    public Transform attackPoint;
+    // public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public int attackDamage = 40;
+
     // Update is called once per frame
     public void Update()
     {
@@ -17,6 +22,13 @@ public class Player_Attack : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             animator.SetTrigger("Attack");
+        }
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, /*attackRange,*/ enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+            Debug.Log("We hit " + enemy.name);
         }
     }
 }
